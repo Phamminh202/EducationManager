47d0b5a793eb01a9556fe952ecb249ff58d349b5
Phamminh202 <73330638+Phamminh202@users.noreply.github.com>
2022-04-13 22:11:12 +0700

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EducationManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EducationManager.WebApiApplication" Language="C#" %>
